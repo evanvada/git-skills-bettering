3b79fadf0bb3449256c2623bed414875dd6f152c
evanvada <evanvadapro@gmail.com>
2024-01-06 17:40:33 +0100

    added a rotating square

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,15 @@
+
+
 function update() {
 	ctx.fillStyle = "rgba(255, 255, 255, 1)";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+	// draw a rotating square
+	ctx.fillStyle = "rgba(0, 0, 0, 1)";
+	ctx.translate(canvas.width/2, canvas.height/2);
+	ctx.rotate(upc/100); // upc = updates count, usede here as a delta time
+	ctx.fillRect(-50, -50, 100, 100);
+	ctx.rotate(-upc/100);
+	ctx.translate(-canvas.width/2, -canvas.height/2);
+
 }
