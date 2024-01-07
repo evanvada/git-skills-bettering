06fc6c72d7f767e0271a73164337004eb0c500cb
evanvada <evanvadapro@gmail.com>
2024-01-07 17:42:49 +0100

    changed some hardcoded values

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,9 @@ function update() {
 	// draw a rotating square
 	ctx.fillStyle = "rgba(0, 0, 0, 1)";
 	ctx.translate(canvas.width/2, canvas.height/2);
-	ctx.rotate(upc/100); // upc = updates count, usede here as a delta time
+	ctx.rotate(upc*0.05); // upc = updates count, used here as a delta time
 	ctx.fillRect(-50, -50, 100, 100);
-	ctx.rotate(-upc/100);
+	ctx.rotate(-upc*0.05);
 	ctx.translate(-canvas.width/2, -canvas.height/2);
 
 }
