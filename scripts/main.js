181653a28a0b3835fd82b71153ccf81e247fe550
evanvada <evanvadapro@gmail.com>
2024-01-12 19:07:44 +0100

    Made triangle slower and square faster

--- scripts/main.js
+++ scripts/main.js
@@ -7,22 +7,22 @@ function update() {
 	// draw a rotating square
 	ctx.fillStyle = "rgba(0, 0, 0, 1)";
 	ctx.translate(canvas.width/2, canvas.height/2);
-	ctx.rotate(upc*0.05); // upc = updates count, used here as a delta time
+	ctx.rotate(upc*0.03); // upc = updates count, used here as a delta time
 	ctx.fillRect(-50, -50, 100, 100);
-	ctx.rotate(-upc*0.05);
+	ctx.rotate(-upc*0.03);
 	ctx.translate(-canvas.width/2, -canvas.height/2);
 
 	// draw a rotating triangle
 	let h = 30*Math.sqrt(3)
 	ctx.fillStyle = "rgba(256, 256, 256, 1)";
 	ctx.translate(canvas.width/2, canvas.height/2);
-	ctx.rotate(upc*0.1);
+	ctx.rotate(upc*0.06);
 	ctx.beginPath();
 	ctx.moveTo(0, -h + h/3);
 	ctx.lineTo(-30, h/3);
 	ctx.lineTo(30, h/3);
 	ctx.fill();
-	ctx.rotate(-upc*0.1);
+	ctx.rotate(-upc*0.06);
 	ctx.translate(-canvas.width/2, -canvas.height/2);
 
 }
